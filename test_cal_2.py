47430fde7f894ad3cd1b8cfe82044fa55a882096
fan <673803577@qq.com>
2020-12-26 18:28:13 +0800

    test_cal_2做了修改，可以调取单独封装读取yam的方法

--- test_cal_2.py
+++ test_cal_2.py
@@ -2,12 +2,15 @@ import pytest
 
 from pythoncode.calculator import Calculator
 import getyam_open_cal
-get_datas = getyam_open_cal
+
+
 ##################  想把读取yam的方法单独封装，还没成功引用
 class TestCalc:
-
+    get_datas = getyam_open_cal.get_datas()
     def setup_class(self):
         self.calc = Calculator()
+
+
 
     def setup_method(self):
         print("\n计算开始")
@@ -16,28 +19,28 @@ class TestCalc:
         print("\n计算结束")
 
     ####加法add
-    @pytest.mark.parametrize("a,b,expect", get_datas()[0], ids=get_datas()[1])
+    @pytest.mark.parametrize("a,b,expect", get_datas[0], ids=get_datas[1])
     def test_add(self, a, b, expect):
         result = self.calc.add(a, b)
         print(result)
         assert result == expect
 
     ####减法sub
-    @pytest.mark.parametrize("a,b,expect", get_datas()[2], ids=get_datas()[3])
+    @pytest.mark.parametrize("a,b,expect", get_datas[2], ids=get_datas[3])
     def test_sub(self, a, b, expect):
         result = self.calc.sub(a, b)
         print(result)
         assert result == expect
 
     ####乘法mul
-    @pytest.mark.parametrize("a,b,expect", get_datas()[4], ids=get_datas()[5])
+    @pytest.mark.parametrize("a,b,expect", get_datas[4], ids=get_datas[5])
     def test_mul(self, a, b, expect):
         result = self.calc.mul(a, b)
         print(result)
         assert result == expect
 
     ####除法div
-    @pytest.mark.parametrize("a,b,expect", get_datas()[6], ids=get_datas()[7])
+    @pytest.mark.parametrize("a,b,expect", get_datas[6], ids=get_datas[7])
     def test_div(self, a, b, expect):
         result = self.calc.div(a, b)
         print(result)
