0cab64293e4812ff1d8eb136c0151f2abb8322cb
fan <673803577@qq.com>
2021-01-06 00:45:00 +0800

    pytest2的作业

---
+++ README.md
@@ -0,0 +1,23 @@
+##霍格沃兹学院pytest课程第一次作业
+
+##作业列表
+-[测试计算机作业]
+- [测试计算机](test_python/test_cal.py)
+- [yam数据](test_python/cal.yml)
+
+#####以下是自己想做的
+
+- [单独做的调用单独封装的读取yam的py](test_python/test_cal_2.py),
+- [单独封装读取yam](test_python/getyam_open_cal.py)
+
+##########pytest课程2的作业
+
+-[测试用例](test_python/test_calc_new.py)
+-[conftest](test_python/conftest.py)
+-[数据](test_python/cal.yml)
+-[测试报告](test_python/allure-report)
+-[报告](test_python/result)
+
+############selenium1作业
+
+-[作业](test_selenium/test_demo.py)
---
+++ test_selenium/test_demo.py
@@ -0,0 +1,87 @@
+import json
+from time import sleep
+
+
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+class TestTestbaidu():
+
+    def setup_method(self):
+
+############################### 用当前已经登录的浏览器打开 ############################
+        ##用当前已经登录的浏览器打开
+        chrome_args = webdriver.ChromeOptions()
+       # chrome_args.debugger_address = "127.0.0.1:9222"
+        self.driver = webdriver.Chrome(options=chrome_args)
+        #self.driver.get("https://work.weixin.qq.com/wework_admin/frame")
+
+#####################################################################################
+
+########################### 读取cookie并打开企业微信放在setup_method里 ###################
+        # # 使用cookie登录前要知道在哪个页面登录的
+        # self.driver.get("https://work.weixin.qq.com/")
+        # # 以文件流的形式打开一个叫cookie.json给他r读的权限
+        # with open("cookie.json", "r") as f:
+        #     # 读取cookie，并把内容赋值给cookie
+        #     cookies = json.load(f)
+        # # 在网页注入cookie
+        # for cookie in cookies:
+        #     self.driver.add_cookie(cookie)
+        #     # 打开企业微信地址
+        #     self.driver.get("https://work.weixin.qq.com/wework_admin/frame")
+        #     sleep(3)
+###################################################################
+
+
+    def teardown_method(self):
+        # pass
+        # 关掉应用程序里打开的Chromedriver
+        self.driver.quit()
+
+    def test_cookie(self):
+
+                #pass
+################################ 这段是获取cookie的 ############
+        # #获取cookie
+        # cookie = self.driver.get_cookies()
+        # #以文件流的形式打开一个叫cookie.json给他w写的权限
+        # with open("cookie.json","w") as f:
+        #     #吧获取到的cookie存储到cookie.json里
+        #     ######print(cookie)
+        #     json.dump(cookie,f)
+#############################################################
+
+############################# 这段是读取cookie的 ##############################
+        #使用cookie登录前要知道在哪个页面登录的
+        self.driver.get("https://work.weixin.qq.com/")
+        #以文件流的形式打开一个叫cookie.json给他r读的权限
+        with open("cookie.json","r") as f:
+            #读取cookie，并把内容赋值给cookie
+            cookies = json.load(f)
+        #在网页注入cookie
+        for cookie in cookies:
+            self.driver.add_cookie(cookie)
+            #打开企业微信地址
+        self.driver.get("https://work.weixin.qq.com/wework_admin/frame")
+        sleep(5)
+##################################################################################
+            # #点击 客户联系
+        self.driver.find_element(By.XPATH, '// *[ @ id = "menu_customer"] / span').click()
+
+            # 等待5秒
+        sleep(5)
+            # 关闭浏览器
+        self.driver.close()
+        print("点击【客户联系】成功")
+
+    def test_qiyeweixn(self):
+        pass
+        # # #点击 客户联系
+        # self.driver.find_element(By.XPATH, "//*[@class='frame_nav_item_title']").click()
+        # # 等待5秒
+        # sleep(5)
+        # #关闭浏览器
+        # self.driver.close()
+        # print("点击【客户联系】成功")
+
